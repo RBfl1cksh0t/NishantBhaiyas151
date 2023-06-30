a5aa8bb208574e3431a14d62fe5e71ef167d4b8b
RB <rajarshibasu19198@gmail.com>
2023-06-30 12:43:08 +0530

    Trying to Commit

--- src/Arrays/RotateArray.java
+++ src/Arrays/RotateArray.java
@@ -1,4 +1,24 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class RotateArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4 , 5,6, 7};
+        System.out.println(Arrays.toString(rotateArray(arr,3)));
+
+    }
+
+    static int[] rotateArray (int[] arr, int k){
+        int[] tempArr = new  int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            tempArr[(i+k)%arr.length] = arr[i];
+        }
+
+        arr = tempArr;
+
+        return arr;
+    }
 }
